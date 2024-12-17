b004c4aa29bd098ba702dc57cc71af0d1d49eb70
kdawg <mcleod.karla@gmail.com>
2024-12-16 22:12:19 -0500

    added changing colors functionality

--- projects/ccl/js/script.js
+++ projects/ccl/js/script.js
@@ -1,9 +1,19 @@
+// Select all small circles, the large circle, and the image-display
 const smallCircles = document.querySelectorAll('.small-circle');
+const largeCircle = document.getElementById('large-circle');
 const displayImage = document.getElementById('display-image');
 
+// Add a click event listener to each small circle
 smallCircles.forEach((circle) => {
   circle.addEventListener('click', () => {
-    const imgSrc = circle.getAttribute('data-img');
-    displayImage.src = imgSrc;
+    // Retrieve URLs from data attributes
+    const largeImageSrc = circle.getAttribute('data-large');
+    const displayImageSrc = circle.getAttribute('data-display');
+
+    // Update the large circle's background image
+    largeCircle.style.backgroundImage = `url(${largeImageSrc})`;
+
+    // Update the display image's src attribute
+    displayImage.src = displayImageSrc;
   });
 });
